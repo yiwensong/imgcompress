fdc441f5ec5aa10d23298a5b6f71201eed2fb598
yiwen song <songzgy@gmail.com>
2016-03-18 19:07:39 -0700

    added support for binaries over 5MB

--- imgcompress.py
+++ imgcompress.py
@@ -10,18 +10,24 @@ import argparse
 import os
 
 WIDTH = 720*3
-# ONETRUEGOD = np.array(Image.open('onetruegod.png'),dtype='uint8')
-# OTG_HEIGHT = ONETRUEGOD.shape[0]
+# Imgur compresses images over 5 MB
+MAX_FILE_SIZE = 5*1000**2
+
+# Size of imgur's encoding
+ENC_LEN = 7
 
 REP = 1
 
-sec = open('secret.secret','r')
-newline_filter = lambda c: False if c=='\n' or c=='\r' else True
-client_id = filter(newline_filter,sec.readline())
-client_secret = filter(newline_filter,sec.readline())
-sec.close()
-client = ImgurClient(client_id,client_secret)
-
+try:
+  sec = open('secret.secret','r')
+  newline_filter = lambda c: False if c=='\n' or c=='\r' else True
+  client_id = filter(newline_filter,sec.readline())
+  client_secret = filter(newline_filter,sec.readline())
+  sec.close()
+  client = ImgurClient(client_id,client_secret)
+except:
+  pass
+  
 def add_zeros(array,rep=REP):
   if rep<=1:
     return array
@@ -33,47 +39,26 @@ def add_zeros(array,rep=REP):
 def rm_zeros(array,rep=REP):
   return np.array([array[i] for i in range(0,array.shape[0],rep)],dtype='uint8')
 
-def topng(path,width=WIDTH,save_path='.tmp.png',mode='RGB'):
-  '''Turns a binary into a nice png'''
-
-  dt = 'uint8'
-  
-  # Get data from binary file
-  binary_array = np.fromfile(path,dtype=dt)
-  # print binary_array.shape
-
-  binary_array.dtype = 'uint8'
-  # print binary_array.shape
-
-  # Used to sparse out data for onetruegod
-  binary_array = add_zeros(binary_array)
+def topng_helper(binary_array,width,save_path,mode):
+  '''Helps turn binaries into nice pngs'''
 
   # find file size
   size_array = np.array([binary_array.shape[0]%256,binary_array.shape[0]/256%256,\
       binary_array.shape[0]/256/256%256,binary_array.shape[0]/256/256/256%256],'uint8')
 
-  sz = 0
-  for i in range(4):
-    sz += size_array[i] * (2**(4*i))
-
   # Add file size to binary
   binary_array = np.concatenate((size_array,binary_array),axis=0)
-
-  # print binary_array.dtype
 
   # Find the height of image and add padding
   data_size = binary_array.shape[0]
   height = data_size/width
   pad = width-data_size%width
-
-  # print size_array, data_size, sz
   
   if pad%width != 0:
     height += 1
   else:
     pad = 0
 
-  # diff = OTG_HEIGHT - (height % OTG_HEIGHT)
   diff = 0
   height += diff
   pad += diff * WIDTH
@@ -90,32 +75,42 @@ def topng(path,width=WIDTH,save_path='.tmp.png',mode='RGB'):
     padding = binary_array[:pad]
   binary_array = np.concatenate((binary_array,padding),axis=0)
 
-  # print 'pad',pad
-  # print height,width,height*width
-  # print len(binary_array)
-
   assert(width*height==binary_array.shape[0])
 
   # Make the matrix of pixels
   dat = binary_array.reshape(height,width/3,3)
-  # dat.dtype = 'uint8'
-
-  # print dat
-  # print len(dat)
-  # print dat.shape
-  # print dat.dtype
-  
-  # gods_needed = height/OTG_HEIGHT
-  # if gods_needed > 0:
-  #   god = np.concatenate([ONETRUEGOD]*gods_needed,axis=0)
 
   image = Image.fromarray(dat,mode=mode)
-  # image = image.convert('RGB')
   image.save(save_path)
-
-  print 'Compression is done. Your compression ratio today was:',data_size/7.0
-
   return save_path
+
+
+def topng(path,width=WIDTH,save_path='.tmp.png',mode='RGB'):
+  '''Turns a binary into a nice png'''
+
+  dt = 'uint8'
+  
+  # Get data from binary file
+  binary_array = np.fromfile(path,dtype=dt)
+  binary_array.dtype = 'uint8'
+
+  # Used to sparse out data for onetruegod
+  binary_array = add_zeros(binary_array)
+
+  size = binary_array.shape[0]
+
+  comp_size = 0
+  ret_path = []
+
+  while comp_size < size:
+    spath = '.' + str(comp_size/MAX_FILE_SIZE) + save_path
+    comp_size += MAX_FILE_SIZE
+    ret_path += [topng_helper(binary_array[:MAX_FILE_SIZE],width,spath,mode)]
+    if comp_size < size:
+      binary_array = binary_array[MAX_FILE_SIZE:]
+
+  print 'Compression is done. Your compression ratio today was:',size/(8.0 * comp_size/MAX_FILE_SIZE)
+  return ret_path
 
 def frompng(png,save_path='BINARY'):
   '''Takes a png file and takes out the binary shit'''
@@ -130,8 +125,9 @@ def frompng(png,save_path='BINARY'):
   # print size
 
   binary_array = binary_array[4:4+size]
-
-  binary_array.tofile(save_path)
+  
+  with open(save_path,'a') as f:
+    f.write(binary_array.tostring())
 
   print 'Decompression is done. You\'re wasting space on your disk now.'
 
@@ -149,21 +145,22 @@ def download(enc,loc):
   return loc
 
 def decompress(path,decomp_path):
-  f = open(path,'r')
-  link = f.read()[:7]
-  png = download(link,'.temp.png')
-  frompng(png,decomp_path)
-  os.remove('.temp.png')
+  with open(path,'r') as f:
+    for line in f:
+      link = line[:7]
+      png = download(link,'.temp.png')
+      frompng(png,decomp_path)
+      os.remove('.temp.png')
 
 def compress(path,comp_path=None):
   if comp_path is None:
     comp_path = path + '.imgc'
-  comp_fd = open(comp_path,'w')
-  tmp_file = topng(path)
-  cb = client.upload_from_path(tmp_file)
-  os.remove(tmp_file)
-  comp_fd.write(str(cb[u'id']))
-  comp_fd.close()
+  with open(comp_path,'w') as compfd:
+    tmp_files = topng(path)
+    for tmp_file in tmp_files:
+      cb = client.upload_from_path(tmp_file)
+      os.remove(tmp_file)
+      comp_fd.write(str(cb[u'id'])+'\n')
 
 def main():
   parser = argparse.ArgumentParser( \
